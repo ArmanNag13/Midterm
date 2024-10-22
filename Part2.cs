31bf718061e82e6af2759a6c39b1a740ccb9d7f7
ArmanNag13 <165388919+ArmanNag13@users.noreply.github.com>
2024-10-22 14:56:05 +0400

    Add files via upload

---
+++ Part2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class Shell
+{
+    private static void ShellSort(int[] elements)
+    {
+        int length = elements.Length;
+
+        for (int distance = length / 2; distance > 0; distance /= 2)
+        {
+            for (int i = distance; i < length; i++)
+            {
+                int keyelement = elements[i];
+                int j;
+
+                for (j = i; j >= distance && elements[j - distance] > keyelement; j -= distance)
+                {
+                    elements[j] = elements[j - distance];
+                }
+                elements[j] = keyelement;
+            }
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        int[] numbers = { 1, 4, 54, 25, 8,697 };
+
+        Console.WriteLine("give");
+        PrintArray(numbers);
+
+        ShellSort(numbers);
+
+        Console.WriteLine("sorted");
+        PrintArray(numbers);
+    }
+
+    public static void PrintArray(int[] array)
+    {
+        foreach (int element in array)
+        {
+            Console.Write( element+ " ");
+        }
+        Console.WriteLine();
+    }
+}
